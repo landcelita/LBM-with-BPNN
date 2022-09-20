ba0b3377456e6a16308f0fe17b2c47ba7c4b54e8
landcelita <nyoo.www@gmail.com>
2022-09-21 05:39:19 +0900

    構造体の定義

--- src/learnableLBM/learnable_lbm.cpp
+++ src/learnableLBM/learnable_lbm.cpp
@@ -58,6 +58,12 @@ struct pyarr2d {
         arr = py::array_t<double>( shape );
     }
 
+    pyarr2d(const py::array_t<double> arr_, const ssize_t forbidden_rows, const ssize_t forbidden_cols) {
+        shape = std::vector<ssize_t>{ arr_.shape(0), arr_.shape(1) };
+        forbidden_at = std::vector<ssize_t>{ forbidden_rows, forbidden_cols };
+        arr = py::array_t<double>(arr_);
+    }
+
     auto& mutable_at(const int h, const int w) {
 #ifdef TEST_MODE
         // 範囲内チェック
@@ -89,7 +95,9 @@ struct InputField {
     pyarr2d u_hori;
     pyarr2d rho;
 
-    InputField(const py::array_t<double> u_vert_, const py::array_t<double> u_hori_, const py::array_t<double> rho_) {
+    InputField(const py::array_t<double> u_vert_, const py::array_t<double> u_hori_, const py::array_t<double> rho_) : 
+    u_vert(u_vert_, 0, 0),  u_hori(u_hori_, 0, 0), rho(rho_, 0, 0), f(u_vert_.shape(0), u_vert_.shape(1), 0, 0, 0.0)
+    {
         if(u_vert_.ndim() != 2 || u_hori_.ndim() != 2 || rho_.ndim() != 2) {
             py::print("ndims of u_vert, u_hori, rho are must be 2, at line", __LINE__);
             throw py::attribute_error();
@@ -98,14 +106,10 @@ struct InputField {
             py::print("shapes of u_vert, u_hori, and rho are must be the same, at line", __LINE__);
             throw py::attribute_error();
         }
-        u_vert = u_vert_;
-        u_hori = u_hori_;
-        rho = rho_;
-        f = pyarr4d(u_vert_.get_shape()[0], u_vert_.get_shape()[1], 0, 0, 0.0);
 
         // f_eqをfとする
-        for(int h = 0; h < f.get_shape()[0]; h++) {
-            for(int w = 0; w < f.get_shape()[1]; w++) {
+        for(int h = 0; h < f.shape[0]; h++) {
+            for(int w = 0; w < f.shape[1]; w++) {
                 double uv = u_vert.at(h, w);
                 double uh = u_hori.at(h, w);
                 for(int dh = 1; dh <= 1; dh++) for(int dw = 1; dw <= 1; dw++) {
@@ -116,21 +120,35 @@ struct InputField {
     }
 };
 
-class StreamingWeight {
-public:
-    StreamingWeight()
-private:
-    pyarr4d w0;
-    pyarr4d w1;
+struct StreamingWeight {
+    pyarr4d w0, w1;
     pyarr4d delta;
-}
+};
+
+struct StreamedField {
+    pyarr4d f;
+    pyarr2d u_vert, u_hori;
+    pyarr2d rho;
+};
+
+struct CollidingWeight {
+    pyarr4d w1, w2, w3, w4;
+    pyarr4d delta;
+};
+
+struct CollidedField {
+    pyarr4d f;
+    pyarr2d u_vert, u_hori;
+    pyarr2d rho;
+    pyarr4d f_eq;
+};
 
 // void LearnableLBM::train(pyarr u_vert, pyarr rho_vert, pyarr u_y) {
 // }
 
 PYBIND11_MODULE(learnableLBM, m) {
-    py::class_<LearnableLBM>(m, "LearnableLBM")
-        .def(py::init<const ssize_t, const ssize_t>())
-        .def("train", &LearnableLBM::train)
-        .def("test", &LearnableLBM::test);
+    // py::class_<LearnableLBM>(m, "LearnableLBM")
+    //     .def(py::init<const ssize_t, const ssize_t>())
+    //     .def("train", &LearnableLBM::train)
+    //     .def("test", &LearnableLBM::test);
 }
