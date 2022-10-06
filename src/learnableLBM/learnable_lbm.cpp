6d52a51e8f471242b369ee32491d10bfdd07c112
landcelita <nyoo.www@gmail.com>
2022-10-07 02:56:17 +0900

    collide() test ok

--- src/learnableLBM/learnable_lbm.cpp
+++ src/learnableLBM/learnable_lbm.cpp
@@ -117,6 +117,96 @@ u_vert(u_vert_, 0, 0),  u_hori(u_hori_, 0, 0), rho(rho_, 0, 0), f(u_vert_.shape(
     }
 };
 
+StreamedField::StreamedField(const ssize_t rows, const ssize_t cols) :
+f(rows, cols, 0, 0, 0.0), u_vert(rows, cols, 0, 0, 0.0), u_hori(rows, cols, 0, 0, 0.0), rho(rows, cols, 0, 0, 0.0) { }
+
+void StreamedField::stream(pyarr4d f_0, pyarr4d w_0, pyarr4d w_1) {
+    if(!(f.shape == f_0.shape && f_0.shape == w_0.shape && w_0.shape == w_1.shape)) {
+        py::print("shapes of f, f_0, w_0, and w_1 are must be the same, at line", __LINE__);
+        py::print("f.shape: ", f.shape[0], f.shape[1], f.shape[2], f.shape[3]);
+        py::print("f_0.shape: ", f_0.shape[0], f_0.shape[1], f_0.shape[2], f_0.shape[3]);
+        py::print("w_0.shape: ", w_0.shape[0], w_0.shape[1], w_0.shape[2], w_0.shape[3]);
+        py::print("w_1.shape: ", w_1.shape[0], w_1.shape[1], w_1.shape[2], w_1.shape[3]);
+        throw py::attribute_error();
+    }
+
+    if(!(f_0.forbidden_at == w_0.forbidden_at && w_0.forbidden_at == w_1.forbidden_at)) {
+        py::print("forbidden_ats of f_0, w_0, and w_1 are must be the same, at line", __LINE__);
+        py::print("f_0.forbidden_at: ", f_0.forbidden_at[0], ", ", f_0.forbidden_at[1]);
+        py::print("w_0.forbidden_at: ", w_0.forbidden_at[0], ", ", w_0.forbidden_at[1]);
+        py::print("w_1.forbidden_at: ", w_1.forbidden_at[0], ", ", w_1.forbidden_at[1]);
+        throw py::attribute_error();
+    }
+
+    f.forbidden_at[0] = f_0.forbidden_at[0] + 1;
+    f.forbidden_at[1] = f_0.forbidden_at[1] + 1;
+    rho.forbidden_at = u_vert.forbidden_at = u_hori.forbidden_at = f.forbidden_at;
+
+    for(int h = f.forbidden_at[0]; h < f.shape[0] - f.forbidden_at[0]; h++) {
+        for(int w = f.forbidden_at[1]; w < f.shape[1] - f.forbidden_at[1]; w++) {
+            double rho_hw = 0.0, rho_u_vert_hw = 0.0, rho_u_hori_hw = 0.0;
+            for(int dh = -1; dh <= 1; dh++) for(int dw = -1; dw <= 1; dw++) {
+                f.mutable_at(h, w, dh, dw) = w_0.at(h, w, dh, dw) + w_1.at(h, w, dh, dw) * f_0.at(h - dh, w - dw, dh, dw);
+                rho_hw += f.at(h, w, dh, dw);
+                rho_u_vert_hw += f.at(h, w, dh, dw) * dh;
+                rho_u_hori_hw += f.at(h, w, dh, dw) * dw;
+            }
+            rho.mutable_at(h, w) = rho_hw;
+            u_vert.mutable_at(h, w) = rho_u_vert_hw / rho_hw;
+            u_hori.mutable_at(h, w) = rho_u_hori_hw / rho_hw;
+        }
+    }
+}
+
+CollidedField::CollidedField(const ssize_t rows, const ssize_t cols) :
+f(rows, cols, 0, 0, 0.0), u_vert(rows, cols, 0, 0, 0.0), u_hori(rows, cols, 0, 0, 0.0), rho(rows, cols, 0, 0, 0.0), f_eq(rows, cols, 0, 0, 0.0) {}
+
+void CollidedField::collide(pyarr4d f_1, pyarr4d w_1, pyarr4d w_2, pyarr4d w_3, pyarr4d w_4) {
+    if(!(f.shape == f_1.shape && f_1.shape == w_1.shape && w_1.shape == w_2.shape && w_2.shape == w_3.shape && w_3.shape == w_4.shape)) {
+        py::print("shapes of f, f_1, w_1, w_2, w_3, w_4 are must be the same, at line", __LINE__);
+        throw py::attribute_error();
+    }
+
+    if(!(f_1.forbidden_at == w_1.forbidden_at && w_1.forbidden_at == w_2.forbidden_at && w_2.forbidden_at == w_3.forbidden_at && w_3.forbidden_at == w_4.forbidden_at)) {
+        py::print("forbidden_ats of f_1, w_1, w_2, w_3 and w_4 are must be the same, at line", __LINE__);
+        throw py::attribute_error();
+    }
+
+    f.forbidden_at = u_vert.forbidden_at = u_hori.forbidden_at = rho.forbidden_at = f_eq.forbidden_at = f_1.forbidden_at;
+
+    for(int h = f.forbidden_at[0]; h < f.shape[0] - f.forbidden_at[0]; h++) {
+        for(int w = f.forbidden_at[1]; w < f.shape[1] - f.forbidden_at[1]; w++) {
+            double rho_1_hw = 0.0, u_vert_1_hw = 0.0, u_hori_1_hw = 0.0;
+            for(int dh = -1; dh <= 1; dh++) for(int dw = -1; dw <= 1; dw++) {
+                rho_1_hw += f_1.at(h, w, dh, dw);
+                u_vert_1_hw += f_1.at(h, w, dh, dw) * dh;
+                u_hori_1_hw += f_1.at(h, w, dh, dw) * dw;
+            }
+            
+            u_vert_1_hw /= rho_1_hw; u_hori_1_hw /= rho_1_hw;
+
+            double rho_hw = 0.0, rho_u_vert_hw = 0.0, rho_u_hori_hw = 0.0;
+
+            for(int dh = -1; dh <= 1; dh++) for(int dw = -1; dw <= 1; dw++) {
+                double vu_1 = u_vert_1_hw * dh + u_hori_1_hw * dw;
+                double vxu_1 = dh * u_hori_1_hw - dw * u_vert_1_hw;
+                double uu_1 = u_vert_1_hw * u_vert_1_hw + u_hori_1_hw * u_hori_1_hw;
+                f_eq.mutable_at(h, w, dh, dw) = C[dh+1][dw+1] * rho_1_hw * (
+                    1.0 + w_1.at(h, w, dh, dw) * vu_1 + w_2.at(h, w, dh, dw) * vxu_1 + w_3.at(h, w, dh, dw) * vu_1 * vu_1 + w_4.at(h, w, dh, dw) * uu_1
+                );
+                double fhw = f.mutable_at(h, w, dh, dw) = 0.5 * (f_1.at(h, w, dh, dw) + f_eq.at(h, w, dh, dw));
+                rho_hw += fhw;
+                rho_u_vert_hw += fhw * dh;
+                rho_u_hori_hw += fhw * dw;
+            }
+
+            rho.mutable_at(h, w) = rho_hw;
+            u_vert.mutable_at(h, w) = rho_u_vert_hw / rho_hw;
+            u_hori.mutable_at(h, w) = rho_u_hori_hw / rho_hw;
+        }
+    }
+}
+
 PYBIND11_MODULE(learnableLBM, m) {
 #ifndef TEST_MODE
 #else
@@ -138,5 +228,23 @@ PYBIND11_MODULE(learnableLBM, m) {
         .def_readwrite("u_hori", &InputField::u_hori)
         .def_readwrite("rho", &InputField::rho)
         .def_readwrite("f", &InputField::f);
+
+    py::class_<StreamedField>(m, "StreamedField")
+        .def(py::init<const ssize_t, const ssize_t>())
+        .def("stream", &StreamedField::stream)
+        .def_readwrite("f", &StreamedField::f)
+        .def_readwrite("u_vert", &StreamedField::u_vert)
+        .def_readwrite("u_hori", &StreamedField::u_hori)
+        .def_readwrite("rho", &StreamedField::rho);
+    
+    py::class_<CollidedField>(m, "CollidedField")
+        .def(py::init<const ssize_t, const ssize_t>())
+        .def("collide", &CollidedField::collide)
+        .def_readwrite("f", &CollidedField::f)
+        .def_readwrite("u_vert", &CollidedField::u_vert)
+        .def_readwrite("u_hori", &CollidedField::u_hori)
+        .def_readwrite("rho", &CollidedField::rho)
+        .def_readwrite("f_eq", &CollidedField::f_eq);
+
 #endif
 }
