83a5ae34e607477db43e8025dd046ccd21757443
landcelita <nyoo.www@gmail.com>
2022-10-22 22:57:12 +0900

    完成

--- src/main.py
+++ src/main.py
@@ -53,25 +53,37 @@ def main():
     u_vert, _, _ = repo.fetchdata(train_datetimes_x[0])
     lbm = LBM.LearnableLBM(u_vert.shape[0], u_vert.shape[1])
 
-    times = 0
+    err_u_vert = err_u_hori = du_vert = du_hori = 0.0
     # このへんのLBMに対する処理はもしかしたら後で切り出すかも
-    for i in range(len(train_datetimes_x)):
-        u_vert_x_nparr, u_hori_x_nparr, pressure_x_nparr = repo.fetchdata(train_datetimes_x[i])
-        u_vert_y_nparr, u_hori_y_nparr, _ = repo.fetchdata(train_datetimes_y[i])
-        u_vert_x_nparr *= -0.01 # 下向き正
-        u_hori_x_nparr *= 0.01 # この定数の根拠はmax_wind_speed.py参照
-        u_vert_y_nparr *= -0.01
-        u_hori_y_nparr *= 0.01
-        u_vert_y = LBM.pyarr2d(u_vert_y_nparr, 2, 2) # この2がマジックナンバーでわかりずらそう
-        u_hori_y = LBM.pyarr2d(u_hori_y_nparr, 2, 2)
-        
-        u_vert_got, u_hori_got = lbm.forward(u_vert_x_nparr, u_hori_x_nparr, pressure_x_nparr)
-        lbm.backward(0.01, u_vert_y, u_hori_y)
+    for i in range(10):
+        for j in range(len(train_datetimes_x)):
+            u_vert_x_nparr, u_hori_x_nparr, pressure_x_nparr = repo.fetchdata(train_datetimes_x[j])
+            u_vert_y_nparr, u_hori_y_nparr, _ = repo.fetchdata(train_datetimes_y[j])
+            u_vert_x_nparr *= -0.01 # 下向き正
+            u_hori_x_nparr *= 0.01 # この定数の根拠はmax_wind_speed.py参照
+            u_vert_y_nparr *= -0.01
+            u_hori_y_nparr *= 0.01
+            u_vert_y = LBM.pyarr2d(u_vert_y_nparr, 2, 2) # この2がマジックナンバーでわかりずらそう
+            u_hori_y = LBM.pyarr2d(u_hori_y_nparr, 2, 2)
+            
+            u_vert_got, u_hori_got = lbm.forward(u_vert_x_nparr, u_hori_x_nparr, pressure_x_nparr)
+            lbm.backward(0.25, u_vert_y, u_hori_y)
 
-        if i % 10 == 0:
             forb = u_vert_got.forbidden_at
-            print(f"{i}: ave diff vert: {100.0 * np.mean(u_vert_got.arr[forb[0]:-forb[0], forb[1],-forb[1]] - u_vert_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]])} m/s")
-            print(f"{i}: ave diff hori: {100.0 * np.mean(u_hori_got.arr[forb[0]:-forb[0], forb[1]:-forb[1]] - u_hori_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]])} m/s")
+            err_u_vert += 100.0 * np.mean(np.abs(u_vert_got.arr[forb[0]:-forb[0], forb[1]:-forb[1]] - u_vert_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]]))
+            err_u_hori += 100.0 * np.mean(np.abs(u_hori_got.arr[forb[0]:-forb[0], forb[1]:-forb[1]] - u_hori_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]]))
+            du_vert += 100.0 * np.mean(np.abs(u_vert_x_nparr[forb[0]:-forb[0], forb[1]:-forb[1]] - u_vert_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]]))
+            du_hori += 100.0 * np.mean(np.abs(u_hori_x_nparr[forb[0]:-forb[0], forb[1]:-forb[1]] - u_hori_y_nparr[forb[0]:-forb[0], forb[1]:-forb[1]]))
+
+            if j % 10 == 9:
+                print(f"epoch {i + 1}, {j + 1}", flush=True)
+                print(f"ave diff vert: {'{:.5f}'.format(err_u_vert / 10.0)} m/s, ave diff hori: {'{:.5f}'.format(err_u_hori / 10.0)} m/s", flush=True)
+                print(f"actual diff vert: {'{:.5f}'.format(du_vert / 10.0)} m/s, actual diff hori: {'{:.5f}'.format(du_hori / 10.0)} m/s", flush=True)
+                print(f"1st / 2nd: {'{:.5f}'.format(err_u_vert / du_vert)}, {'{:.5f}'.format(err_u_hori / du_hori)}", flush=True)
+                
+
+                err_u_vert = err_u_hori = du_vert = du_hori = 0.0
+
 
     
 if __name__ == "__main__":
